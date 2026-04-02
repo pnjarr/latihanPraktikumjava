b015fc90f6e039a7ff382af011bfce25441e5216
pnjarr <putrishallisaa@gmail.com>
2026-04-02 11:46:38 +0700

    fix

--- latihanPraktikum.java
+++ latihanPraktikum.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class latihanPraktikum {
+    //latihan praktikum
 public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
     int jumlahMatkul;
